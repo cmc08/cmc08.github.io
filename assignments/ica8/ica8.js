6405d015eb5424721ec58e010401ddf2571f88bb
cmc08 <camc7698@colorado.edu>
2022-10-12 22:49:34 -0600

    ica8

--- assignments/ica8/ica8.js
+++ assignments/ica8/ica8.js
@@ -1,6 +1,18 @@
-let body = document.querySelector("div");
-body.addEventListener("click", giveAlert());
-
-function giveAlert() {
-    alert("this is an alert");
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "https://cmc08.github.io/img/charmander.png"){
+        document.getElementById("imgClickAndChange").src = "https://cmc08.github.io/img/charizard.png";
+    } else {
+        document.getElementById("imgClickAndChange").src = "https://cmc08.github.io/img/charmander.png";
+    }
 }
+
+const section = document.getElementById("section");
+
+section.addEventListener("mouseover", function handleMouseOver() {
+  section.style.color = "red";
+});
+
+section.addEventListener("mouseout", function handleMouseOut() {
+  section.style.color = "black";
+});
+
